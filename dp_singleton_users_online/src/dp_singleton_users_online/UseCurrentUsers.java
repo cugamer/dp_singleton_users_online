d7f6b61fa0a2e8d096542d432958cf6780ea5d68
Jonathon Nordquist <cugamer@gmail.com>
2016-03-19 13:42:18 -0500

    Update UseCurrentUsers class.

--- dp_singleton_users_online/src/dp_singleton_users_online/UseCurrentUsers.java
+++ dp_singleton_users_online/src/dp_singleton_users_online/UseCurrentUsers.java
@@ -6,16 +6,20 @@ public class UseCurrentUsers {
 
 	public static void main(String[] args) {
 		CurrentUsersOnline currentUsersOnline = CurrentUsersOnline.getCurrentUsersOnline();
+		LinkedList<Integer> currentList = currentUsersOnline.getCurrentOnlineUserIds();
 		
 		currentUsersOnline.addUserId(52);
 		currentUsersOnline.addUserId(198);
 		currentUsersOnline.addUserId(42);
-		LinkedList<Integer> currentList = currentUsersOnline.getCurrentOnlineUserIds();
-		System.out.println(currentList);
+		System.out.println(currentList + " current list after intitial creation and addition");
+		
+		CurrentUsersOnline currentUsersOnline2 = CurrentUsersOnline.getCurrentUsersOnline();
+		LinkedList<Integer> currentList2 = currentUsersOnline.getCurrentOnlineUserIds();
+		System.out.println(currentList2 + " current list after attempting to instantiate the class a second time");
 		
 		currentUsersOnline.addUserId(42);
 		currentUsersOnline.addUserId(43);
-		System.out.println(currentList);
+		System.out.println(currentList + " current list after using the addUserId method");
 		
 		try{
 			currentUsersOnline.removeUserId(199);
@@ -29,7 +33,7 @@ public class UseCurrentUsers {
 			System.out.println("Item 198 not found");
 		}
 		
-		System.out.println(currentList);
+		System.out.println(currentList + " current list after using the removeUserId method.");
 	}
 
 }
