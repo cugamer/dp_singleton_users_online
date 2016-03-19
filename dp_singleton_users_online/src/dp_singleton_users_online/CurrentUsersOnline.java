18259fdf8bbd71c0b7aaef431becaf2d11113b92
Jonathon Nordquist <cugamer@gmail.com>
2016-03-19 13:58:59 -0500

    Double-checked locking to threadsafe the getCurrentUsersOnline method.

--- dp_singleton_users_online/src/dp_singleton_users_online/CurrentUsersOnline.java
+++ dp_singleton_users_online/src/dp_singleton_users_online/CurrentUsersOnline.java
@@ -3,14 +3,18 @@ package dp_singleton_users_online;
 import java.util.LinkedList;
 
 public class CurrentUsersOnline {
-	private static CurrentUsersOnline currentUsersOnline;
+	private volatile static CurrentUsersOnline currentUsersOnline;
 	private LinkedList<Integer> currentUserList = new LinkedList<>();
 	
 	private CurrentUsersOnline(){}
 	
 	public static CurrentUsersOnline getCurrentUsersOnline(){
-		if(currentUsersOnline == null){
-			return new CurrentUsersOnline();
+		if(currentUsersOnline == null){ 			 // Check for an instance, if there is none enter the synch block
+			synchronized(CurrentUsersOnline.class){  // Synch on the first go through
+				if(currentUsersOnline == null){      // Once synched across threads, check for an instance again and create if needed
+					return new CurrentUsersOnline();
+				}
+			}
 		}
 		return currentUsersOnline;
 	}
